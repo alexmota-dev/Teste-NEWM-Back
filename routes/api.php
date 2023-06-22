f1b447113de9d03c94963f3b3cdf032461a6ec4d
Alex Mota <alexmota@alu.ufc.br>
2023-06-22 18:53:09 -0300

    renomeando class client na api

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('v1/funcionario', [EmployeeController::class, 'index']);
-Route::post('v1/funcionario', [EmployeeController::class, 'store']);
-Route::get('v1/funcionario/{id}', [EmployeeController::class, 'show']);
-Route::delete('v1/funcionario/{id}', [EmployeeController::class, 'destroy']);
-Route::put('v1/funcionario/{id}', [EmployeeController::class, 'update']);
+Route::get('v1/funcionario', [ClientController::class, 'index']);
+Route::post('v1/funcionario', [ClientController::class, 'store']);
+Route::get('v1/funcionario/{id}', [ClientController::class, 'show']);
+Route::get('v1/searchforclientsbyemail/{email}', [ClientController::class, 'SearchFor10UsersByEmail']);
+Route::get('v1/searchforclientsbyname/{name}', [ClientController::class, 'SearchFor10UsersByName']);
+Route::delete('v1/funcionario/{id}', [ClientController::class, 'destroy']);
+Route::put('v1/funcionario/{id}', [ClientController::class, 'update']);
 
