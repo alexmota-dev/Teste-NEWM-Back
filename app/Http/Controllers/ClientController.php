072048ba52012622444d0eaaff3ecb97f5c026c2
Alex Mota <alexmota@alu.ufc.br>
2023-06-23 20:08:32 -0300

    alterando respostas da api para retornar mensagens para o frontend

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -20,18 +20,13 @@ class ClientController extends Controller
     }
     public function index(){
         //abrindo a conexão com o banco
-        $testConnection = new Connect;
-        // $cmd = $testConnection->connection->query($query);
-        //selecionando todos os funcionairos
+        $startConnection = new Connect;
         $query = "SELECT * FROM clients";
-        $cmd = $testConnection->connection->query($query);
+        $cmd = $startConnection->connection->query($query);
+        //fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
         //convertendo para obj
         $clients = $cmd->fetchAll(PDO::FETCH_OBJ);
-        //fechando a conexão com o banco
-        // $this->closeConnection($testConnection);
-        // return $clients;
-        //qual a dirença exata de usar um return ou die ?
-        // $this->addHeaders();
         return(json_encode($clients));
     }
 
@@ -40,7 +35,6 @@ class ClientController extends Controller
             return $cpf;
     }
     public function store(){
-        // $body = (json_decode(file_get_contents('php://input'),false))->body;
         $body = json_decode(file_get_contents('php://input'), true);
         if(isset($body['observation'])){
             $observation = $body["observation"];
@@ -57,33 +51,59 @@ class ClientController extends Controller
             $address = $body["address"];
             $birthArray = explode("-",$birth);
             if($birthArray[1] > 12){
-                http_response_code(400);
-                // $this->addHeaders();
-                die("O mês não pode ultrapassar 12");
+                $response = array(
+                    'status' => 400,
+                    'message' => 'O mês não pode ultrapassar 12'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
             }
             if($birthArray[2] > 31){
                 http_response_code(400);
-                // $this->addHeaders();
-                die("O dia não pode ultrapassar 31");
+                $response = array(
+                    'status' => 400,
+                    'message' => 'O dia não pode ultrapassar 31'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
             }
         }
         else{
-            http_response_code(400);
-            die("Faltam dados na requisição. Verifique os campos.");
-        }
-        if(strlen($phone) > 11 || strlen($cpf) > 11){
-            http_response_code(400);
-            die("Verifique se os campos CPF/Telefone tem um tamanho adequado.");
+            $response = array(
+                'status' => 400,
+                'message' => 'Faltam dados na requisição. Verifique os campos.'
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
 
-        $pdo = new Connect();
+        $startConnection = new Connect();
         $existisFuncionario = $this->findByEmail($email);
         if($existisFuncionario){
-            http_response_code(409);
-            die("Existe funcionario com o email ". $email);
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 409,
+                'message' => "Já existe funcionario com o email".$email
+            );
+            $json_response = json_encode($response);
+            return $json_response;
+        }
+        //Validando cpf antes de verificar tamanho
+        $cpf = $this->verifyCPF($cpf);
+        //verifica o tamanho de todos os campos
+        if(strlen($phone) > 11 || strlen($cpf) > 11 || strlen($name) > 50 || strlen($email) > 50 || strlen($address) > 50 || strlen($observation) > 300){
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 400,
+                'message' => "Verifique o tamanho dos campos."
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
         $query = "INSERT INTO clients (name, birth, phone, cpf, email, address, observation) VALUES (:name, :birth, :phone, :cpf, :email, :address, :observation)";
-        $cmd = $pdo->connection->prepare($query);
+        $cmd = $startConnection->connection->prepare($query);
 
         $cmd->bindValue(":name",$name);
         $cmd->bindValue(":birth",$birth);
@@ -95,122 +115,101 @@ class ClientController extends Controller
         $cmd->bindValue(":observation",$observation, PDO::PARAM_NULL);
 
         $cmd->execute();
-
-        $this->closeConnection($pdo);
-        http_response_code(200);
-        // $this->addHeaders();
-        die("User criado !");
+        //fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
+        $response = array(
+            'status' => 200,
+            'message' => "User criado."
+        );
+        $json_response = json_encode($response);
+        return $json_response;
     }
 
     public function show($id){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=localhost', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        // esse trecho se repete no show e no destroy
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE id = :id');
-        // SELECT * FROM clients WHERE name LIKE CONCAT(:name, '%');
-        // ^^^^^^
-        // na busca pelo name eu posso usar isso
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE id = :id';
+        $cmd = $startConnection->connection->prepare($query);
 
         $cmd->bindValue(":id",$id);
         $cmd->execute();
+        // fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
         $funcionario = $cmd->fetch(PDO::FETCH_OBJ);
         if($funcionario){
-            $this->closeConnection($pdo);
             http_response_code(200);
             $this->addHeaders();
             die(json_encode($funcionario));
         }
         else{
-            $this->closeConnection($pdo);
             http_response_code(404);
             $this->addHeaders();
-            die("Não existe funcionario com o id ". $id);
+            $response = array(
+                'status' => 404,
+                'message' => "Não existe funcionario com esse id."
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
-        //consigo acessar $funcionario->name, formato: Obj.
     }
 
     public function findByEmail($email){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=localhost', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        // esse trecho se repete no show e no destroy
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE email = :email');
-        // SELECT * FROM clients WHERE name LIKE CONCAT(:name, '%');
-        // na busca pelo name eu posso usar isso
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE email = :email';
+        $cmd = $startConnection->connection->prepare($query);
         $cmd->bindValue(":email",$email);
         $cmd->execute();
+        // fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
         $funcionario = $cmd->fetch(PDO::FETCH_OBJ);
         if($funcionario){
-            $this->closeConnection($pdo);
             http_response_code(200);
             // $this->addHeaders();
             return $funcionario;
         }
         else{
-            $this->closeConnection($pdo);
             http_response_code(404);
             // $this->addHeaders();
             return 0;
         }
-        //consigo acessar $funcionario->name, formato: Obj.
     }
 
     public function SearchFor10UsersByEmail($email){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=localhost', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        // esse trecho se repete no show e no destroy
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE email LIKE :email LIMIT 10');
-        // SELECT * FROM clients WHERE name LIKE CONCAT(:name, '%');
-        // ^^^^^^
-        // na busca pelo name eu posso usar isso
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE email LIKE :email LIMIT 10';
+        $cmd = $startConnection->connection->prepare($query);
+
         $cmd->bindValue(":email",$email."%");
         $cmd->execute();
+        // fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
         $funcionario = $cmd->fetchAll(PDO::FETCH_OBJ);
         if($funcionario){
-            $this->closeConnection($pdo);
             http_response_code(200);
             // $this->addHeaders();
             die(json_encode($funcionario));
         }
         else{
-            $this->closeConnection($pdo);
             http_response_code(404);
             // $this->addHeaders();
             die("Não existe funcionario com o email ". $email);
         }
-        //consigo acessar $funcionario->name, formato: Obj.
     }
 
     public function SearchFor10UsersByName($name){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=localhost', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        // esse trecho se repete no show e no destroy
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE name LIKE :name LIMIT 10');
-        // SELECT * FROM clients WHERE name LIKE CONCAT(:name, '%');
-        // ^^^^^^
-        // na busca pelo name eu posso usar isso
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE name LIKE :name LIMIT 10';
+        $cmd = $startConnection->connection->prepare($query);
         $cmd->bindValue(":name",$name."%");
         $cmd->execute();
+        // fechando conexão por uma questão de segurança
+        $this->closeConnection($startConnection);
         $funcionario = $cmd->fetchAll(PDO::FETCH_OBJ);
         if($funcionario){
-            $this->closeConnection($pdo);
             http_response_code(200);
             // $this->addHeaders();
             die(json_encode($funcionario));
         }
         else{
-            $this->closeConnection($pdo);
             http_response_code(404);
             // $this->addHeaders();
             die("Não existe funcionario com o name ". $name);
@@ -219,49 +218,54 @@ class ClientController extends Controller
     }
 
     public function destroy($id){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=127.0.0.1', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE id = :id');
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE id = :id';
+        $cmd = $startConnection->connection->prepare($query);
         $cmd->bindValue(":id",$id);
         $cmd->execute();
         $userExists = $cmd->fetch();
         $cmd = null;
 
         if($userExists){
-            $cmd = $pdo->prepare("DELETE FROM clients WHERE id = :id");
+            $cmd = $startConnection->connection->prepare("DELETE FROM clients WHERE id = :id");
             $cmd->bindValue(":id",$id);
             $cmd->execute();
-            $this->closeConnection($pdo);
-            // $this->addHeaders();
-            die("O funcionario de id ".$id." foi removido.");
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 200,
+                'message' => "User deletado com sucesso."
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }else {
-            //posso fazer uma função pra executar essas 3 linhas abaixo, elas se repetem
-            $this->closeConnection($pdo);
-            http_response_code(404);
-            // $this->addHeaders();
-            die("Não existe funcionario com o id ". $id);
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 200,
+                'message' => "Não existe user com o id". $id
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
     }
 
     public function update($id){
-        try {
-            $pdo = new PDO('mysql:dbname=terceirotestenewm; host=localhost', "root", "root");
-        } catch (\PDOException $pdoError) {
-            throw $pdoError;
-        }
-        $cmd = $pdo->prepare('SELECT * FROM clients WHERE id = :id');
+        $startConnection = new Connect();
+        $query = 'SELECT * FROM clients WHERE id = :id';
+        $cmd = $startConnection->connection->prepare($query);
         $cmd->bindValue(":id",$id);
         $cmd->execute();
         $userExists = $cmd->fetch();
-        $cmd = null;
         if(!$userExists){
-            $this->closeConnection($pdo);
-            http_response_code(404);
-            $this->addHeaders();
-            die("Não existe funcionario com o id ". $id);
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 404,
+                'message' => "Não existe funcionario com o id ". $id
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
 
         $jsonObj = json_decode(file_get_contents('php://input'), true);
@@ -277,25 +281,52 @@ class ClientController extends Controller
             $birthArray = explode("-",$birth);
 
             if($birthArray[1] > 12){
-                $this->closeConnection($pdo);
-                http_response_code(400);
-                // $this->addHeaders();
-                die("O mês não pode ultrapassar 12");
+                // fechando conexão por uma questão de segurança
+                $this->closeConnection($startConnection);
+                $response = array(
+                    'status' => 400,
+                    'message' => 'O mês não pode ultrapassar 12'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
             }
             if($birthArray[2] > 31){
-                $this->closeConnection($pdo);
-                http_response_code(400);
-                // $this->addHeaders();
-                die("O dia não pode ultrapassar 31");
+                // fechando conexão por uma questão de segurança
+                $this->closeConnection($startConnection);
+                $response = array(
+                    'status' => 400,
+                    'message' => 'O dia não pode ultrapassar 31'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
             }
             $alreadyExistsUserWithEmail = $this->findByEmail($email);
             if($alreadyExistsUserWithEmail && $alreadyExistsUserWithEmail->email != $userExists["email"] ){
-                http_response_code(409);
-                die("Já existe user com este email !");
+                // fechando conexão por uma questão de segurança
+                $this->closeConnection($startConnection);
+                $response = array(
+                    'status' => 409,
+                    'message' => 'Já existe user com este email !'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
             }
             $cpf = $this->verifyCPF($cpf);
-            var_dump($cpf);
-            $cmd = $pdo->
+
+            //verifica o tamanho de todos os campos
+            if(strlen($phone) > 11 || strlen($cpf) > 11 || strlen($name) > 50 || strlen($email) > 50 || strlen($address) > 50 || strlen($observation) > 300){
+                // fechando conexão por uma questão de segurança
+                $this->closeConnection($startConnection);
+                $response = array(
+                    'status' => 400,
+                    'message' => 'Verifique o tamanho dos campos !'
+                );
+                $json_response = json_encode($response);
+                return $json_response;
+
+            }
+
+            $cmd = $startConnection->connection->
             prepare(
                     "UPDATE clients
                     SET name = :name, birth = :birth, phone = :phone, cpf = :cpf, email = :email, address = :address, observation = :observation
@@ -312,15 +343,24 @@ class ClientController extends Controller
             $cmd->bindValue(":id",$id);
             $cmd->execute();
 
-            $this->closeConnection($pdo);
-            http_response_code(200);
-            $this->addHeaders();
-            die("Funcionario atualizado com sucesso.");
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            $response = array(
+                'status' => 200,
+                'message' => 'Funcionario atualizado com sucesso.'
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }else {
-            $this->closeConnection($pdo);
-                http_response_code(400);
-                $this->addHeaders();
-                die("Solicitação incorreta, corpo da requisição está errado.");
+            // fechando conexão por uma questão de segurança
+            $this->closeConnection($startConnection);
+            http_response_code(400);
+            $response = array(
+                'status' => 200,
+                'message' => 'Solicitação incorreta, corpo da requisição está ausente.'
+            );
+            $json_response = json_encode($response);
+            return $json_response;
         }
     }
 }
